55a4bc79caedc3b2aa827f651295ba9c3d586217
Dan Kiss <flowfast47@gmail.com>
2019-03-12 16:58:51 -0700

    use url to grab params for express

--- express_test/index.js
+++ express_test/index.js
@@ -18,6 +18,19 @@ app.get('/contact', function(req, res) {
     res.send('this is the contact page');
 });
 
+var students = {
+    1 : 'Mark',
+    2: 'Tom',
+    3: 'john'
+}
+
+//we are passing parameters via the route
+//:colon before parameter name to get the parameter
+//req.params.id can we used to query a database
+app.get('/students/:id', function(req, res) {
+    res.send('you have requested student name ' + students[req.params.id]);
+});
+
 //app is created an will listen on port 3000
 app.listen(3000, function(){
     console.log('Server live on port 3000')
